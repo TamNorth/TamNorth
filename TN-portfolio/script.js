35bee62d5d058cc01ed73f1224e93743d277dd2c
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-07-29 14:59:20 +0100

    add rotating jib to crane, with rortation tied to cursor.x

--- TN-portfolio/script.js
+++ TN-portfolio/script.js
@@ -1,5 +1,16 @@
 import * as THREE from "three";
 
+// CURSOR
+const cursor = {
+  x: 0,
+  y: 0,
+};
+
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.width - 0.5);
+});
+
 // BASE
 // Canvas
 const canvas = document.querySelector("canvas.webgl");
@@ -8,19 +19,58 @@ const canvas = document.querySelector("canvas.webgl");
 const scene = new THREE.Scene();
 
 // OBJECTS
+// Crane Group
 const crane = new THREE.Group();
+crane.position.x = 5;
 scene.add(crane);
 const craneMaterial = new THREE.MeshBasicMaterial({
   color: 0xff0000,
   wireframe: true,
 });
+const craneSizes = {
+  width: 1,
+  mast: {
+    length: 10,
+  },
+  jib: {
+    length: 10,
+  },
+};
 
-const pillar = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 5, 1, 1, 5, 1),
+// Mast Mesh
+const mast = new THREE.Mesh(
+  new THREE.BoxGeometry(
+    craneSizes.width,
+    craneSizes.mast.length,
+    craneSizes.width,
+    craneSizes.width,
+    craneSizes.mast.length,
+    craneSizes.width
+  ),
   craneMaterial
 );
+mast.position.y = craneSizes.mast.length / 2;
+crane.add(mast);
 
-crane.add(pillar);
+// Jib Group
+const jibGroup = new THREE.Group();
+jibGroup.position.y = craneSizes.mast.length + craneSizes.width / 2;
+crane.add(jibGroup);
+
+// Jib Mesh
+const jib = new THREE.Mesh(
+  new THREE.BoxGeometry(
+    craneSizes.jib.length,
+    craneSizes.width,
+    craneSizes.width,
+    craneSizes.jib.length,
+    craneSizes.width,
+    craneSizes.width
+  ),
+  craneMaterial
+);
+jib.position.x = -(craneSizes.jib.length / 5);
+jibGroup.add(jib);
 
 // SIZES
 const sizes = {
@@ -46,7 +96,8 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   100
 );
-camera.position.z = 5;
+camera.position.z = craneSizes.mast.length;
+camera.position.y = craneSizes.mast.length / 2 + 2;
 scene.add(camera);
 
 // RENDERER
@@ -61,6 +112,11 @@ const clock = new THREE.Clock();
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
+
+  //Update objects
+  jibGroup.rotation.y = cursor.x * Math.PI + 0.25 * Math.PI;
+
+  // Render
   renderer.render(scene, camera);
   window.requestAnimationFrame(tick);
 };
