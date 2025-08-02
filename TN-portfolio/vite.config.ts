f435c60d7b97fa79e5df3613f182a04cf5b24be3
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-08-02 11:28:48 +0100

    initialise tailwindcss

--- TN-portfolio/vite.config.ts
+++ TN-portfolio/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,5 +12,5 @@ export default defineConfig({
     emptyOutDir: true, // Empty the folder first
     sourcemap: true, // Add sourcemap
   },
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
 });
