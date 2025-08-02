07fd2c658e02c8234913b10302cdc464eee54710
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-08-02 12:30:09 +0100

    apply initial styling to header

--- TN-portfolio/src/components/sections/Header.tsx
+++ TN-portfolio/src/components/sections/Header.tsx
@@ -1,3 +1,7 @@
 export default function Header() {
-  return <p>Under construction...</p>;
+  return (
+    <header className="group fixed inset-x-0 top-0 flex justify-center items-center bg-transparent">
+      <h1 className="group-hover:underline text-white text-2xl">Tam North</h1>
+    </header>
+  );
 }
