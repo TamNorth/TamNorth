849ab9083f44d9dac3a832fc51157e40c5b6305e
Demim0nde <tamnorth.github.37eup@slmails.com>
2025-08-03 22:52:29 +0100

    move header external links to dedicated buttons

--- TN-portfolio/src/components/sections/Header.tsx
+++ TN-portfolio/src/components/sections/Header.tsx
@@ -1,19 +1,21 @@
 import { useEffect, useRef, useState } from "react";
+import { HiExternalLink } from "react-icons/hi";
 import {
   MdOutlineVisibility,
   MdOutlineVisibilityOff,
   MdOutlineMenu,
 } from "react-icons/md";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 export default function Header() {
   const [visibility, setVisibility] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
   const [showAbout, setShowAbout] = useState(false);
-  const [showLinks, setShowLinks] = useState(false);
+  const [showContact, setShowContact] = useState(false);
   const aboutRef = useRef<null | HTMLButtonElement>(null);
   const linksRef = useRef<null | HTMLButtonElement>(null);
   const aboutDropdownRef = useRef<null | HTMLElement>(null);
-  const linksDropdownRef = useRef<null | HTMLUListElement>(null);
+  const contactsDropdownRef = useRef<null | HTMLUListElement>(null);
 
   useEffect(() => {
     document.body.addEventListener("click", (event: MouseEvent) => {
@@ -28,19 +30,20 @@ export default function Header() {
       if (
         linksRef.current &&
         !event.composedPath().includes(linksRef.current) &&
-        linksDropdownRef.current &&
-        !event.composedPath().includes(linksDropdownRef.current)
+        contactsDropdownRef.current &&
+        !event.composedPath().includes(contactsDropdownRef.current)
       ) {
-        setShowLinks(false);
+        setShowContact(false);
       }
     });
   }, []);
 
-  const buttonSize = 25;
+  const buttonSize = "25";
   const buttonPadding = "p-6";
-  const headerOpacity = "20";
+  const headerOpacityStyle = `opacity-20 group-hover:opacity-100 transition duration-500`;
   const subMenuStyle =
     "mt-3 *:pt-2 *:nth-1:pt-0 text-left rounded-2xl bg-black/20 backdrop-blur-sm p-5";
+  const buttonStyle = "hover:scale-110 transition cursor-pointer";
   const logo = (
     <a
       href="/"
@@ -55,7 +58,7 @@ export default function Header() {
 
   return (
     <header
-      className={`group fixed inset-x-0 top-0 flex justify-between items-center text-white *:transition font-cinzel pb-10 ${
+      className={`group fixed inset-x-0 top-0 grid gap-20 grid-cols-[1fr_1fr_auto_1fr_1fr] justify-items-center items-center text-white *:transition font-cinzel pb-10 ${
         visibility &&
         `bg-radial-[at_50%_0%] from-black/30 from-45% to-transparent transition to-70%`
       }`}
@@ -67,20 +70,15 @@ export default function Header() {
           setShowMenu(!showMenu);
         }}
         className={`hover:scale-110 ${buttonPadding} ${
-          visibility ||
-          showMenu ||
-          `opacity-${headerOpacity} group-hover:opacity-100`
+          visibility || showMenu || headerOpacityStyle
         }`}
       >
         <MdOutlineMenu size={buttonSize} />
       </button>
       <nav
         aria-label="Main menu"
-        className={`absolute left-1/2 -translate-x-1/2 ${
-          visibility ||
-          showAbout ||
-          showLinks ||
-          `opacity-${headerOpacity} group-hover:opacity-100`
+        className={`col-start-3 ${
+          visibility || showAbout || showContact || headerOpacityStyle
         }`}
       >
         <ul className="grid grid-cols-[1fr_auto_1fr] gap-20 text-center items-center">
@@ -91,7 +89,7 @@ export default function Header() {
                 setShowAbout(!showAbout);
               }}
               ref={aboutRef}
-              className="hover:scale-110 transition cursor-pointer"
+              className={buttonStyle}
             >
               about
             </button>
@@ -101,13 +99,13 @@ export default function Header() {
                 ref={aboutDropdownRef}
               >
                 <h2 className="text-center">Welcome to my site!</h2>
-                <p className="text-justify">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt,
-                  cupiditate. Perspiciatis commodi nulla nobis quos, recusandae
-                  sed veritatis beatae voluptates. Voluptatem laborum cum
-                  sapiente itaque beatae consequuntur reprehenderit! Suscipit,
-                  laborum!
-                </p>
+                <p className="text-justify"></p>
+                <ul>
+                  <li>Projects</li>
+                  <li>Skills</li>
+                  <li>Education</li>
+                  <li>Experience</li>
+                </ul>
               </article>
             )}
           </li>
@@ -116,26 +114,43 @@ export default function Header() {
             <button
               type="button"
               onClick={() => {
-                setShowLinks(!showLinks);
+                setShowContact(!showContact);
               }}
               ref={linksRef}
-              className="hover:scale-110 transition cursor-pointer"
+              className={buttonStyle}
             >
-              links
+              contact
             </button>
-            {showLinks && (
-              <ul className={`absolute ${subMenuStyle}`} ref={linksDropdownRef}>
-                <li>
-                  <a href="https://github.com/TamNorth">github</a>
-                </li>
-                <li>
-                  <a href="https://linkedin.com/in/t-n-ba5a003b/">linkedin</a>
-                </li>
-              </ul>
-            )}
+            {showContact && <></>}
           </li>
         </ul>
       </nav>
+      <ul
+        aria-label="External links"
+        className={`*:inline *:ml-3 *:nth-1:ml-0`}
+        ref={contactsDropdownRef}
+      >
+        <li>
+          <a href="https://github.com/TamNorth">
+            <FaGithub
+              className={`inline mr-1 ${buttonStyle} ${
+                visibility || headerOpacityStyle
+              }`}
+              size={buttonSize}
+            />
+          </a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/t-n-ba5a003b/">
+            <FaLinkedin
+              className={`inline mr-1 ${buttonStyle} ${
+                visibility || headerOpacityStyle
+              }`}
+              size={buttonSize}
+            />
+          </a>
+        </li>
+      </ul>
       <button
         type="button"
         aria-label={`toggle header visibility aid ${visibility ? "off" : "on"}`}
