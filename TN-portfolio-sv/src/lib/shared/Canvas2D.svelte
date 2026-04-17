e376a50b44d7440c19d647108b0f98b21401a76f
TamNorth <tamnorth.github.37eup@slmails.com>
2026-04-17 21:34:50 +0100

    provide props on Canvas

--- TN-portfolio-sv/src/lib/shared/Canvas2D.svelte
+++ TN-portfolio-sv/src/lib/shared/Canvas2D.svelte
@@ -1,13 +1,34 @@
-<canvas>
+<script lang="js">
+	import { onMount } from 'svelte';
 
-</canvas>
+	let { canvasFn = () => {}, height = "", width = "", ...attrs } = $props();
+
+    const style = `
+        --height: ${height || '100vh'}; 
+        --width: ${width || '100vw'};
+    `
+	let canvas = $state();
+	let mousePosition = $state({ x: 0, y: 0 });
+	const handleMouseMove = (e) => {
+		mousePosition.x = e.clientX;
+		mousePosition.y = e.clientY;
+	};
+
+	onMount(() => {
+		canvasFn(canvas, mousePosition);
+	});
+</script>
+
+<svelte-css-wrapper {style} >
+	<canvas bind:this={canvas} onmousemove={handleMouseMove} {...attrs}></canvas>
+</svelte-css-wrapper>
 
 <style>
-    canvas {
-        border: solid var(--text-colour-base);
-        position: fixed;
+	canvas {
+		border: solid var(--text-colour-base);
+		position: fixed;
 		background-color: var(--background-colour-card);
-        height: 100vh;
-        width: 100vw;
-    }
+		height: var(--height);
+		width: var(--width);
+	}
 </style>
