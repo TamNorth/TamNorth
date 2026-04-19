303ef58294a969551d4a6d33572708139ca4cce9
TamNorth <tamnorth.github.37eup@slmails.com>
2026-04-19 02:31:17 +0100

    grid | merge partners

--- TN-portfolio-sv/src/routes/grid/+page.svelte
+++ TN-portfolio-sv/src/routes/grid/+page.svelte
@@ -3,7 +3,7 @@
 	import Canvas2D from '$lib/shared/Canvas2D/Canvas2D.svelte';
 	import { paintShapes } from '$lib/shared/Canvas2D/utils.js';
 
-	const GRID_SIZE = 2
+	const GRID_SIZE = 4
 
 	const rowHeightScaleFactor = Math.sin(Math.PI * (2 / 3));
 	const fillWithCount = (array: number[], start: number = 0) => array.fill(0).map((_, i) => i + start);
@@ -33,15 +33,15 @@
 
 			const v1 = {
 				x: x,
-				y: isUp ? y + yScale : y - yScale
+				y: isUp ? y - yScale : y + yScale
 			};
 			const v2 = {
 				x: x + xScale,
-				y: isUp ? y - yScale : y + yScale
+				y: isUp ? y + yScale : y - yScale
 			};
 			const v3 = {
 				x: x - xScale,
-				y: isUp ? y - yScale : y + yScale
+				y: isUp ? y + yScale : y - yScale
 			};
 
 			return [v1, v2, v3];
@@ -58,7 +58,7 @@
 
 			// check if triangle should point up or down
 			const isOdd = !!(triangleIndex % 2);
-			const isUp = Math.sign(y) < 0 ? isOdd : !isOdd;
+			const isUp = Math.sign(y) < 0 ? !isOdd : isOdd;
 
 			const triangle = drawTriangle(isUp, { x, y });
 
@@ -68,17 +68,14 @@
 		}, []);
 	}
 
-	console.log(getHexgridTriangles(GRID_SIZE).map(({id}) => id));
-
-	function selectMergingPartners(shapes, _remainingIndices: number[] = []) {
-		console.log(shapes)
-		const shapesCopy = structuredClone(shapes)
+	function selectMergingPartners(shapes, _remainingIndices: number[] = [], _mergedShapes = []) {
+		// const shapesCopy = structuredClone(shapes)
 		_remainingIndices = _remainingIndices.length ? _remainingIndices : fillWithCount(Array(shapes.length))
 		const selectorNum = Math.floor(Math.random() * _remainingIndices.length)
 		const t1Index = _remainingIndices[selectorNum]
 
-		const t1 = shapesCopy[t1Index]
-		const {x: x1, y: y1} = t1.id
+		const t1 = shapes[t1Index]
+		const {isUp, id: {x: x1, y: y1}} = t1
 
 		function selectPartner(previousTries = []) {
 			if (previousTries.length >= 3) return {}
@@ -89,25 +86,41 @@
 			const x2 = direction === "right" ? x1 + 1 : direction === "left" ? x1 - 1 : x1;
 			const y2 = direction === "down" ? y1 + 1 : direction === "up" ? y1 - 1 : y1;
 
-			const t2Index = shapesCopy.findIndex(({id}) => id.x === x2 && id.y === y2)
+			const t2Index = shapes.findIndex(({id}) => id.x === x2 && id.y === y2)
 
-			return t2Index !== -1 && !shapesCopy[t2Index].mergePartner ? {t2: shapesCopy[t2Index], t2Index} : selectPartner([...previousTries, direction])
+			return t2Index !== -1 && _remainingIndices.includes(t2Index) ? {t2: shapes[t2Index], t2Index, direction} : selectPartner([...previousTries, direction])
 		}
 
-		const {t2, t2Index} = selectPartner()
+		const {t2, t2Index, direction} = selectPartner()
 
 		if (!t2) {
-			t1.mergePartner = null
 			const newRemainingIndices = _remainingIndices.filter(index => index != t1Index)
-			return newRemainingIndices.length ? selectMergingPartners(shapesCopy, newRemainingIndices) : shapesCopy
+			return newRemainingIndices.length ? selectMergingPartners(shapes, newRemainingIndices, [..._mergedShapes, t1.vertices]) : [..._mergedShapes, t1.vertices]
 		}
 
-		t1.mergePartner = t2.id
-		t2.mergePartner = t1.id
+		const [t1v1, t1v2, t1v3] = t1.vertices
+		const [t2v1, t2v2, t2v3] = t2.vertices 
+
+		const mergedVertices = (() => {
+			switch (direction) {
+				case "left":
+					return isUp 
+						? [t2v3, t1v1, t1v2, t1v3]
+						: [t1v3, t1v2, t1v1, t2v3]
+				case "right":
+					return isUp
+						? [t1v1, t2v2, t1v2, t1v3]
+						: [t1v3, t1v2, t2v2, t1v1]
+				case "up":
+					return [t2v1, t1v2, t1v1, t1v3]
+				case "down":
+					return [t1v1, t1v2, t2v1, t1v3]
+			}
+		})()
 
 		const newRemainingIndices = _remainingIndices.filter(index => ![t1Index, t2Index].includes(index))
 
-		return newRemainingIndices.length ? selectMergingPartners(shapesCopy, newRemainingIndices) : shapesCopy
+		return newRemainingIndices.length ? selectMergingPartners(shapes, newRemainingIndices, [..._mergedShapes, mergedVertices]) : [..._mergedShapes, mergedVertices]
 	}
 
 	function mergeShapes(shape1, shape2) {
@@ -116,10 +129,11 @@
 	const shapes = getHexgridTriangles(GRID_SIZE)
 	const shapesVertices = shapes.map(({vertices}) => vertices)
 
-	selectMergingPartners(shapes)
+	const mergedShapes = selectMergingPartners(shapes)
+	console.log(mergedShapes)
 
 	const canvasFn = (canvas) => {
-		paintShapes(canvas, shapesVertices, 'red');
+		paintShapes(canvas, mergedShapes, undefined);
 	}
 </script>
 
