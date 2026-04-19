5363c1458d98d85e7ab6147809ba3620c67a2b93
TamNorth <tamnorth.github.37eup@slmails.com>
2026-04-19 21:39:17 +0100

    shared | add Tile component

---
+++ TN-portfolio-sv/src/lib/shared/Tile.svelte
@@ -0,0 +1,14 @@
+<script>
+	let { children = () => {} } = $props();
+</script>
+
+<div class="tile">
+	{@render children()}
+</div>
+
+<style>
+	.tile {
+		padding: var(--base-spacing);
+		background-color: var(--background-colour-tile);
+	}
+</style>
