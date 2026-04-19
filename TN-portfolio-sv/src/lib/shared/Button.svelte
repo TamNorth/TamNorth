43b0069a033e74fd5b470251de74b74ae0432af3
TamNorth <tamnorth.github.37eup@slmails.com>
2026-04-19 23:18:08 +0100

    shared | add Button component

---
+++ TN-portfolio-sv/src/lib/shared/Button.svelte
@@ -0,0 +1,15 @@
+<script>
+	let { children = () => {}, onClick = () => {} } = $props();
+</script>
+
+<button onclick={onClick}>
+	{@render children()}
+</button>
+
+<style>
+	button {
+		padding: var(--base-spacing);
+		background-color: var(--background-colour-button);
+		cursor: pointer;
+	}
+</style>
