57bd46be7989e539362318178cbd18b76a12f317
TamNorth <tamnorth.github.37eup@slmails.com>
2026-04-19 18:55:45 +0100

    Canvas2D | provide mouse click position to canvasFn

--- TN-portfolio-sv/src/lib/shared/Canvas2D/Canvas2D.svelte
+++ TN-portfolio-sv/src/lib/shared/Canvas2D/Canvas2D.svelte
@@ -10,23 +10,30 @@
 
 	let canvas = $state();
 	let mousePosition = $state({ x: 0, y: 0 });
+	let mouseClick = $state({ x: 0, y: 0 });
+
 	const handleMouseMove = (e) => {
 		mousePosition.x = e.clientX;
 		mousePosition.y = e.clientY;
+	};
+
+	const handleMouseClick = (e) => {
+		mouseClick.x = e.clientX;
+		mouseClick.y = e.clientY;
 	};
 
 	// let canvasHeight = $state()
 	// let canvasWidth = $state()
 
 	onMount(() => {
-		canvasFn(canvas, mousePosition);
+		canvasFn({canvas, mousePosition, mouseClick});
 		// canvasHeight = window.innerHeight
 		// canvasWidth = window.innerWidth
 	});
 </script>
 
 <svelte-css-wrapper {style}>
-	<canvas bind:this={canvas} height={1000} width={2000} onmousemove={handleMouseMove} {...attrs}
+	<canvas bind:this={canvas} height={1000} width={2000} onmousemove={handleMouseMove} onclick={handleMouseClick}
 	></canvas>
 </svelte-css-wrapper>
 
