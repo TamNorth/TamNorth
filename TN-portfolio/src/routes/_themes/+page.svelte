9ffe55f4998443befc2a2702bb4b14201d5b6c9a
TamNorth <tamnorth.github.37eup@slmails.com>
2026-04-20 14:34:01 +0100

    _themes | create route

---
+++ TN-portfolio/src/routes/_themes/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Tile from '$lib/shared/Tile.svelte';
+	import Card from '$lib/shared/Card.svelte';
+	import Light from '$lib/shared/Light.svelte';
+	import Link from '$lib/shared/Link.svelte';
+	import Page from '$lib/shared/Page.svelte';
+</script>
+
+<Page>
+	<Card visible={false}>
+		<Tile><Light colour="red" size={1}></Light></Tile>
+	</Card>
+</Page>
