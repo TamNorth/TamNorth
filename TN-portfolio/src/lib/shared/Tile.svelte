04e8501fecc32c05fef7cf6241cd78df472f2b30
TamNorth <tamnorth.github.37eup@slmails.com>
2026-04-20 15:27:43 +0100

    Tile | move styles to cockpit stylesheet

--- TN-portfolio/src/lib/shared/Tile.svelte
+++ TN-portfolio/src/lib/shared/Tile.svelte
@@ -8,24 +8,8 @@
 
 <style>
 	.bevel {
-		--shadow-length: 2px;
-		--shadow-direction-x: -1;
-		--shadow-direction-y: 1;
-		--external-shadow:
-			rgb(0, 0, 0, 50%) calc(var(--shadow-length) * var(--shadow-direction-x))
-				calc(var(--shadow-length) * var(--shadow-direction-y)) 3px 0,
-			rgb(0, 0, 0, 50%) 0 0 1px 1px;
-
-		--inset-highlight-colour: 255, 200, 150;
-		--bevel-size: 2px;
-		--bevel-blur: var(--bevel-size);
-
-		border-radius: calc(var(--bevel-size) * 0.5);
-		box-shadow:
-			var(--external-shadow),
-			inset rgb(var(--inset-highlight-colour), 20%) calc(var(--bevel-size) * -1) var(--bevel-size)
-				var(--bevel-blur) 0,
-			inset rgb(0, 0, 0, 50%) var(--bevel-size) calc(var(--bevel-size) * -1) var(--bevel-blur) 0;
+		border-radius: var(--border-radius-tile);
+		box-shadow: var(--bevel-and-shadow);
 
 		padding: var(--base-spacing);
 		background-color: var(--background-colour-tile);
