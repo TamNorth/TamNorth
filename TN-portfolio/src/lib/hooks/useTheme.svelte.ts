c55b6e56f3c67bc847dbdf08201bb658891ed72b
TamNorth <tamnorth.github.37eup@slmails.com>
2026-05-05 23:03:08 +0100

    useTheme | convert to TS

--- TN-portfolio/src/lib/hooks/useTheme.svelte.ts
+++ TN-portfolio/src/lib/hooks/useTheme.svelte.ts
@@ -1,8 +1,8 @@
 import { themes } from '../constants/styles/themes/themes.js';
 
-export default function useTheme(themeName = null, isDarkMode = true) {
+export default function useTheme(themeName: string | null = null, isDarkMode: boolean = true) {
 	const themeNames = Object.keys(themes);
-	let currentThemeName = themeName ?? themeNames[0];
+	const currentThemeName = themeName ?? themeNames[0];
 	const currentTheme = $derived(themes[currentThemeName]?.[isDarkMode ? 'dark' : 'light']);
 
 	return { themeNames, currentTheme };
